339681c8ed7285e34eefc06823151e232fa70818
Mka91 <kiwogens@yandex.ru>
2020-09-30 15:43:45 +0700

    Range

--- Range/src/ru/academits/maksimenko/range/Range.java
+++ Range/src/ru/academits/maksimenko/range/Range.java
@@ -1,4 +1,68 @@
 package ru.academits.maksimenko.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public Range getIntersection(Range range) {
+        if (range.to <= from || to <= range.from) {
+            return null;
+        }
+
+        return new Range(Math.max(from, range.from), Math.min(to, range.to));
+    }
+
+    public Range[] getMerger(Range range) {
+        if (range.to < from || to < range.from) {
+            return new Range[]{new Range(from, to), new Range(range.from, range.to)};
+        }
+
+        return new Range[]{new Range(Math.min(from, range.from), Math.max(to, range.to))};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (range.from > from && range.to < to) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+        }
+
+        if (range.from > from && range.to >= to && range.from < to) {
+            return new Range[]{new Range(from, range.from)};
+        }
+
+        if (range.from <= from && range.to < to && range.to > from) {
+            return new Range[]{new Range(range.to, to)};
+        }
+
+        if (range.from <= from && to <= range.to) {
+            return new Range[0];
+        }
+
+        return new Range[]{new Range(from, to)};
+    }
+
+    @Override
+    public String toString() {
+        return "{ " + "from = " + from + ", to = " + to + " }";
+    }
 }
--- Range/src/ru/academits/maksimenko/range_main/Main.java
+++ Range/src/ru/academits/maksimenko/range_main/Main.java
@@ -1,7 +1,21 @@
 package ru.academits.maksimenko.range_main;
 
+import ru.academits.maksimenko.range.Range;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Range range1 = new Range(4, 9);
+        Range range2 = new Range(2, 39);
 
+        Range intersection = range1.getIntersection(range2);
+        System.out.println("Интервал пересечения: " + intersection);
+
+        Range[] merger = range1.getMerger(range2);
+        System.out.println("Объединение интервалов: " + Arrays.toString(merger));
+
+        Range[] difference = range1.getDifference(range2);
+        System.out.println("Раность интервалов: " + Arrays.toString(difference));
     }
 }
