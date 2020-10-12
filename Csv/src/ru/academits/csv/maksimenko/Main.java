f13664d78f3f8c6f9891e145d62a5e741dbdf611
Mka91 <kiwogens@yandex.ru>
2020-10-12 15:33:49 +0700

    CSV

--- Csv/src/ru/academits/csv/maksimenko/Main.java
+++ Csv/src/ru/academits/csv/maksimenko/Main.java
@@ -1,17 +1,29 @@
 package ru.academits.csv.maksimenko;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String line = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("Таблицааа.csv"));
+             PrintWriter printWriter = new PrintWriter("Таблица.html")) {
+            String line;
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("Таблица.csv"))) {
+            printWriter.print("<table>");
+
             while ((line = bufferedReader.readLine()) != null) {
-                String[] csv = line.split(",");
-                System.out.println(Arrays.toString(csv));
+                String[] csv = line.replaceAll("<", "&lt").replaceAll(">", "&gt").
+                        replaceAll("&", "&amp").split(",");
+
+                printWriter.print("<tr>");
+
+                for (String s : csv) {
+                    printWriter.printf("<td>%s</td> ", s);
+                }
+
+                printWriter.print("</tr><br/>");
             }
+
+            printWriter.print("</table>");
         } catch (IOException e) {
             e.printStackTrace();
         }
