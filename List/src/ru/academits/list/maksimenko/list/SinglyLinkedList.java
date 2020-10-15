eae4d3391cc8cdb45920528be3487094a0edf840
Mka91 <kiwogens@yandex.ru>
2020-10-15 21:03:36 +0700

    List

---
+++ List/src/ru/academits/list/maksimenko/list/SinglyLinkedList.java
@@ -0,0 +1,134 @@
+package ru.academits.list.maksimenko.list;
+
+public class SinglyLinkedList<T> {
+    private ListItem<T> head;
+    private int count;
+
+    public SinglyLinkedList() {
+    }
+
+    public SinglyLinkedList(ListItem<T> head) {
+        this.head = head;
+    }
+
+    public int getSize() {
+        return count;
+    }
+
+    public T getFirsData() {
+        return head.getData();
+    }
+
+    private ListItem<T> getItemByIndex(int index) {
+        ListItem<T> item = null;
+        int i = 0;
+
+        for (ListItem<T> current = head; current != null; current = current.getNext()) {
+            if (index == i) {
+                item = current;
+            }
+
+            ++i;
+        }
+
+        return item;
+    }
+
+    public T getDataByIndex(int index) {
+        ListItem<T> item = getItemByIndex(index);
+
+        return item.getData();
+    }
+
+    public T setDataByIndex(int index, T data) {
+        ListItem<T> item = getItemByIndex(index);
+
+        T oldValue = item.getData();
+
+        item.setData(data);
+
+        return oldValue;
+    }
+
+    public T deleteItemByIndex(int index) {
+        T data;
+
+        if (index == 0) {
+            data = head.getData();
+
+            deleteFirstElement();
+
+            return data;
+        }
+
+        ListItem<T> previous = getItemByIndex(index - 1);
+        ListItem<T> current = previous.getNext();
+
+        data = current.getData();
+
+        previous.setNext(current.getNext());
+
+        return data;
+    }
+
+    public void insertByBeginning(T data) {
+        head = new ListItem<>(data, head);
+    }
+
+    public void insertByIndex(int index, T data) {
+        if (index == 0) {
+            insertByBeginning(data);
+        }
+
+        ListItem<T> newItem = new ListItem<T>(data);
+        ListItem<T> previous = getItemByIndex(index - 1);
+
+        newItem.setNext(previous.getNext().getNext());
+        previous.setNext(newItem);
+    }
+
+    public boolean deleteItemByValue(T data) {
+        for (ListItem<T> current = head; current != null; current = current.getNext()) {
+            if (data.equals(current.getData())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public T deleteFirstElement() {
+        T data = head.getData();
+
+        head = head.getNext();
+
+        return data;
+    }
+
+    public void reverse() {
+        ListItem<T> previous = null;
+        ListItem<T> current = head;
+
+        while (current != null) {
+            ListItem<T> next = current.getNext();
+
+            current.setNext(previous);
+            previous = current;
+            current = next;
+        }
+
+        head = previous;
+    }
+
+    public SinglyLinkedList<T> copy() {
+        SinglyLinkedList<T> copy = new SinglyLinkedList<>(new ListItem<>(head.getData()));
+
+        for (ListItem<T> current = head, currentCopy = copy.head; current != null; current = current.getNext(), currentCopy = currentCopy.getNext()) {
+            ListItem<T> copyItem = new ListItem<>(current.getData());
+
+            currentCopy.setNext(copyItem);
+        }
+
+        return copy;
+    }
+}
---
+++ List/src/ru/academits/list/maksimenko/list_main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.list.maksimenko.list_main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
