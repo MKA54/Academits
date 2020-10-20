a0529a496f396685db8f922340d051e9423175a3
Mka91 <kiwogens@yandex.ru>
2020-10-20 14:59:57 +0700

    ArrayList

--- ArrayList/src/ru/academits/array_list/maksimenko/array_list/MyArrayList.java
+++ ArrayList/src/ru/academits/array_list/maksimenko/array_list/MyArrayList.java
@@ -52,7 +52,7 @@ public class MyArrayList<T> implements List<T> {
     public Iterator<T> iterator() {
         return new Iterator<>() {
             int i = 0;
-            int modification = currentModification;
+            final int modification = currentModification;
 
             @Override
             public boolean hasNext() {
