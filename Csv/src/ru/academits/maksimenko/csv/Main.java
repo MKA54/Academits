d1d66a440b3d957a6e8ad836986a23c65a8345db
Mka91 <kiwogens@yandex.ru>
2020-10-25 21:09:48 +0700

    Csv

--- Csv/src/ru/academits/maksimenko/csv/Main.java
+++ Csv/src/ru/academits/maksimenko/csv/Main.java
@@ -4,28 +4,87 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("Таблица.csv"));
-             PrintWriter printWriter = new PrintWriter("Таблица.html")) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]));
+             PrintWriter printWriter = new PrintWriter(args[1])) {
             String line;
 
             printWriter.print("<table>");
 
+            int m = 0;
+
             while ((line = bufferedReader.readLine()) != null) {
-                String[] csv = line.replaceAll("<", "&lt").replaceAll(">", "&gt").
-                        replaceAll("&", "&amp").split(",");
+                StringBuilder csv = new StringBuilder();
 
                 printWriter.print("<tr>");
 
-                for (String s : csv) {
-                    printWriter.printf("<td>%s</td> ", s);
+                char split = ',';
+                char quote = '"';
+                char less = '<';
+                char more = '>';
+                char amp = '&';
+
+                int lineLength = line.length();
+
+                for (int i = m, j = 1; i < lineLength; i++, j++) {
+                    if ( i == 0){
+                        csv.append("<td>");
+                    }
+
+                    char current = line.charAt(i);
+                    char next = 0;
+
+                    if (j != lineLength) {
+                        next = line.charAt(j);
+                    }
+
+                    if (current == quote && next == split) {
+                        current = next;
+
+                        i += 2;
+                        j += 2;
+                    }
+
+                    if (current == quote && next == quote) {
+                        continue;
+                    }
+
+
+                    if (current == less && next != quote) {
+                        csv.append("&lt;");
+                        continue;
+                    }
+
+                    if (current == more && next != quote) {
+                        csv.append("&gt;");
+                        continue;
+                    }
+
+                    if (current == amp && next != quote) {
+                        csv.append("&amp;");
+                        continue;
+                    }
+
+                    if (current == split && next != quote) {
+                        csv.append("</td>");
+                        m = i;
+
+                        continue;
+                    }
+
+                    csv.append(current);
                 }
 
-                printWriter.print("</tr><br/>");
+                if (m == lineLength) {
+                    printWriter.printf("%s </tr><br/>", csv);
+
+                    m = 0;
+                }
+
             }
 
             printWriter.print("</table>");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Файл не найден");
         }
     }
 }
