6ebf6058241555b8d22f657e3a9e1b94ed66dd30
Mka91 <kiwogens@yandex.ru>
2020-11-12 18:59:33 +0700

    Tree

---
+++ Tree/src/ru/academits/maksimenko/tree/Tree.java
@@ -0,0 +1,291 @@
+package ru.academits.maksimenko.tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Tree<T extends Comparable<T>> {
+    private TreeNode<T> root;
+    private int size;
+
+    /**
+     * @noinspection unused
+     */
+    public Tree() {
+    }
+
+    public Tree(T data) {
+        root = new TreeNode<>(data);
+
+        ++size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public TreeNode<T> getRoot() {
+        return root;
+    }
+
+    public void add(T data) {
+        if (size == 0) {
+            root = new TreeNode<>(data);
+
+            ++size;
+
+            return;
+        }
+
+        TreeNode<T> current = root;
+        TreeNode<T> newNode = new TreeNode<>(data);
+
+        while (true) {
+            if (newNode.compareTo(current) < 0) {
+                if (current.getLeft() == null) {
+                    current.setLeft(newNode);
+
+                    break;
+                }
+
+                current = current.getLeft();
+
+                continue;
+            }
+
+            if (current.getRight() == null) {
+                current.setRight(newNode);
+
+                break;
+            }
+
+            current = current.getRight();
+        }
+
+        ++size;
+    }
+
+    public boolean contains(Object o) {
+        //noinspection unchecked
+        TreeNode<T> node = new TreeNode<>((T) o);
+
+        for (TreeNode<T> current = root; current != null; ) {
+            if (node.compareTo(current) == 0) {
+                return true;
+            }
+
+            if (node.compareTo(current) < 0) {
+                if (current.getLeft() == null) {
+                    return false;
+                }
+
+                current = current.getLeft();
+
+                continue;
+            }
+
+            if (current.getRight() == null) {
+                return false;
+            }
+
+            current = current.getRight();
+        }
+
+        return false;
+    }
+
+    public boolean remove(Object o) {
+        final int initialSize = size;
+
+        //noinspection unchecked
+        TreeNode<T> node = new TreeNode<>((T) o);
+
+        for (TreeNode<T> current = root, previous = null; current != null; ) {
+            if (node.compareTo(current) == 0 && current.getLeft() == null && current.getRight() == null) {
+                if (current == root) {
+                    size--;
+
+                    root = null;
+
+                    break;
+                }
+
+                assert previous != null;
+                if (node.compareTo(previous) < 0) {
+                    previous.setLeft(null);
+                } else {
+                    previous.setRight(null);
+                }
+
+                size--;
+
+                break;
+            }
+
+            if (node.compareTo(current) == 0 && current.getLeft() != null && current.getRight() != null) {
+                TreeNode<T> leftParent = null;
+                TreeNode<T> leftmost = current.getRight();
+
+                while (leftmost.getLeft() != null) {
+                    leftParent = leftmost;
+
+                    leftmost = leftmost.getLeft();
+                }
+
+                if (leftmost.getRight() != null && leftParent != null) {
+                    leftParent.setLeft(leftmost.getRight());
+                }
+
+                if (leftmost.getRight() == null && leftParent != null) {
+                    leftParent.setLeft(null);
+                }
+
+                if (current == root) {
+                    root = leftmost;
+
+                    leftmost.setLeft(current.getLeft());
+
+                    if (current.getRight() == leftmost) {
+                        leftmost.setRight(null);
+                    } else {
+                        leftmost.setRight(current.getRight());
+                    }
+
+                    size--;
+
+                    break;
+                }
+
+                assert previous != null;
+                if (node.compareTo(previous) < 0) {
+                    previous.setLeft(leftmost);
+                } else {
+                    previous.setRight(leftmost);
+                }
+
+                leftmost.setLeft(current.getLeft());
+                leftmost.setRight(current.getRight());
+
+                size--;
+
+                break;
+            }
+
+            if (node.compareTo(current) == 0 && current.getLeft() != null) {
+                if (current == root) {
+                    root = current.getLeft();
+
+                    --size;
+
+                    break;
+                }
+
+                assert previous != null;
+                previous.setLeft(current.getLeft());
+
+                --size;
+
+                break;
+            }
+
+            if (node.compareTo(current) == 0 && current.getRight() != null) {
+                if (current == root) {
+                    root = current.getRight();
+
+                    --size;
+
+                    break;
+                }
+
+                assert previous != null;
+                previous.setRight(current.getRight());
+
+                --size;
+
+                break;
+            }
+
+            previous = current;
+
+            if (node.compareTo(current) < 0) {
+                current = current.getLeft();
+
+                continue;
+            }
+
+            current = current.getRight();
+        }
+
+        return initialSize != size;
+    }
+
+    /**
+     * @noinspection unchecked
+     */
+    public void visitInWidth() {
+        if (size == 0) {
+            System.out.println("There are no elements in the tree");
+
+            return;
+        }
+
+        Queue<T> queue = new LinkedList<>();
+
+        queue.add((T) root);
+
+        while (!queue.isEmpty()) {
+            TreeNode<T> current = (TreeNode<T>) queue.remove();
+
+            if (current.getLeft() != null) {
+                queue.add((T) current.getLeft());
+            }
+
+            if (current.getRight() != null) {
+                queue.add((T) current.getRight());
+            }
+
+            System.out.println(current.getData());
+        }
+    }
+
+    public void visitRecursionInDepth(TreeNode<T> node) {
+        System.out.println(node.getData());
+
+        if (node.getLeft() != null) {
+            visitRecursionInDepth(node.getLeft());
+        }
+
+        if (node.getRight() != null) {
+            visitRecursionInDepth(node.getRight());
+        }
+    }
+
+    /**
+     * @noinspection unchecked
+     */
+    public void visitInDepth() {
+        if (size == 0) {
+            System.out.println("There are no elements in the tree");
+
+            return;
+        }
+
+        Deque<T> stack = new LinkedList<>();
+
+        stack.addLast((T) root);
+
+        while (!stack.isEmpty()) {
+            TreeNode<T> current = (TreeNode<T>) stack.removeLast();
+
+            if (current.getRight() != null) {
+                stack.add((T) current.getRight());
+            }
+
+            if (current.getLeft() != null) {
+                stack.add((T) current.getLeft());
+            }
+
+            System.out.println(current.getData());
+        }
+    }
+}
---
+++ Tree/src/ru/academits/maksimenko/tree_main/Main.java
@@ -0,0 +1,43 @@
+package ru.academits.maksimenko.tree_main;
+
+import ru.academits.maksimenko.tree.Tree;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree<Integer> numbers = new Tree<>(15);
+
+        numbers.add(13);
+        numbers.add(28);
+        numbers.add(11);
+        numbers.add(16);
+        numbers.add(25);
+        numbers.add(31);
+        numbers.add(9);
+        numbers.add(15);
+        numbers.add(29);
+        numbers.add(35);
+        numbers.add(12);
+        numbers.add(30);
+
+        System.out.println("Количество элементов в дереве: " + numbers.getSize());
+
+        boolean contains = numbers.contains(25);
+        System.out.println("Наличие числа в дереве: " + contains);
+
+        boolean remove = numbers.remove(45);
+        System.out.println("Проверка на удаление числа из дереве: " + remove);
+
+        System.out.println("Количество элементов в дереве: " + numbers.getSize());
+
+        System.out.println("Обход дерева в глубину с рекурсией:");
+        numbers.visitRecursionInDepth(numbers.getRoot());
+        System.out.println();
+
+        System.out.println("Обход дерева в глубину:");
+        numbers.visitInDepth();
+        System.out.println();
+
+        System.out.println("Обход дерева в ширину:");
+        numbers.visitInWidth();
+    }
+}
