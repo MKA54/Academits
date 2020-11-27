179bd4bfe190d57136202bb2dde0e97f113f63b1
Mka91 <kiwogens@yandex.ru>
2020-11-27 20:35:24 +0700

    Reflection

---
+++ Reflection/src/ru/academits/maksimenko/classes/House.java
@@ -0,0 +1,54 @@
+package ru.academits.maksimenko.classes;
+
+public class House {
+    private double length;
+    private double width;
+    private String color;
+
+    public House(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public House(double length, double width, String color) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double houseArea(int length, int width) {
+        return length * width;
+    }
+
+    public String[] getRecommendedColors() {
+        return new String[]{
+                "Brown",
+                "White",
+                "Gray"
+        };
+    }
+}
