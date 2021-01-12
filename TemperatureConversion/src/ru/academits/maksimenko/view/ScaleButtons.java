6fd108aee12e570f45e69aa861ed1246fffa1f7e
MKA <67032796+MKA54@users.noreply.github.com>
2021-01-12 20:16:07 +0700

    Delete ScaleButtons.java

--- TemperatureConversion/src/ru/academits/maksimenko/view/ScaleButtons.java
+++
@@ -1,8 +0,0 @@
-package ru.academits.maksimenko.view;
-
-public class ScaleButtons implements ScaleButtonsList {
-    @Override
-    public String[] addTemperatureScale() {
-        return new String[]{"Цельсий", "Кельвин", "Фаренгейт"};
-    }
-}
